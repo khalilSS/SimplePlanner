1de18952a9fd672ee662427f1d94dfc912968e05
keerthanap24 <kpullela24@gmail.com>
2020-11-16 16:15:11 -0800

    Unit 11- Keerthana (also added updated gif)

---
+++ app/src/main/java/com/example/simpleplanner/CalendarActivity.java
@@ -0,0 +1,33 @@
+package com.example.simpleplanner;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CalendarView;
+
+import java.util.Calendar;
+
+public class CalendarActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+        CalendarView calendar;
+
+        calendar = (CalendarView) findViewById(R.id.calendar);
+
+    }
+    public void AddCalendarEvent(View view) {
+        Calendar calendarEvent = Calendar.getInstance();
+        Intent i = new Intent(Intent.ACTION_EDIT);
+        i.setType("vnd.android.cursor.item/event");
+        i.putExtra("beginTime", calendarEvent.getTimeInMillis());
+        i.putExtra("allDay", true);
+        i.putExtra("rule", "FREQ=YEARLY");
+        i.putExtra("endTime", calendarEvent.getTimeInMillis() + 60 * 60 * 1000);
+        i.putExtra("title", "Calendar Event");
+        startActivity(i);
+    }
+}
